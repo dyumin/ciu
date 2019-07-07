b5e87391776900caa76f1708114735e5c857a7fb
Alexey Dyumin <jejni.j@gmail.com>
2019-07-07 13:44:51 +0300

    quick-find algorithm impl

--- Algorithms in C++, Parts 1-4/Program 1.1/main.cpp
+++ Algorithms in C++, Parts 1-4/Program 1.1/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
+static const int N = 5;
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int i, p, q, id[N];
+
+    for (i = 0; i < N; i++)
+        id[i] = i;
+
+    while (std::cin >> p >> q)
+    {
+        const int t = id[p];
+
+        if (t == id[q])
+            continue;
+
+        for (i = 0; i< N; i++)
+        {
+            if (id[i] == t)
+                id[i] = id[q];
+        }
+
+        std::cout << "\t" << p << "\t" << q << std::endl;
+    }
+
+    std::cout << std::endl;
+
     return 0;
 }
