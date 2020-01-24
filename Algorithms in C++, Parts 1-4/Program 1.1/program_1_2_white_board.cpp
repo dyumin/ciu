283cb585c70768eab1827da7e14795d85fbcd54e
Dyumin <jejni.j@gmail.com>
2020-01-24 22:09:45 +0300

    program_1_2_white_board

---
+++ Algorithms in C++, Parts 1-4/Program 1.1/program_1_2_white_board.cpp
@@ -0,0 +1,36 @@
+#include <array>
+#include <iostream>
+
+int main()
+{
+    using arT = std::array<int, 19>;
+    arT conn;
+    using vT = arT::value_type;
+    using sT = arT::size_type;
+    for (sT i = 0; i < conn.size(); i++)
+        conn[i] = i;
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (1)
+    {
+        sT f = 0, t = 0;
+        std::cin >> f >> t;
+
+        auto fEnd = conn[f];
+        for (; conn[fEnd] != conn[conn[fEnd]]; fEnd = conn[fEnd]);
+        auto tEnd = conn[t];
+        for (; conn[tEnd] != conn[conn[tEnd]]; tEnd = conn[tEnd]);
+
+        if (fEnd == tEnd)
+        {
+            std::cout << "already connected" << std::endl;
+            continue;
+        }
+
+        conn[fEnd] = tEnd;
+        std::cout << f << " - " << t << std::endl;
+    }
+#pragma clang diagnostic pop
+    return 0;
+}
